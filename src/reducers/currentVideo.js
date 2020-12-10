10931909c8a8e30303a01b9948168299c3219392
Marlon Esparza <marlonaesparza@gmail.com>
2020-12-10 13:13:14 -0800

    Reducers complete- now to work on combining them

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,8 +1,11 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
-};
+var currentVideoReducer = (state = null, action) => {
+  if (action.type === 'CHANGE_VIDEO') {
+    return action.video;
+  }
 
-//TODO: define a reducer for the currentVideo field of our state.
+  return state;
+};
 
 export default currentVideoReducer;
