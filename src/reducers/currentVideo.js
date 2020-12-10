22d2f3571fcc6f5e3bee4f150e032439b61feb07
Marlon Esparza <marlonaesparza@gmail.com>
2020-12-09 19:09:28 -0800

    Complete planning state

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,8 @@
 import Redux from 'redux';
 
 var currentVideoReducer = (state, action) => {
-  //TODO: define a reducer for the currentVideo field of our state.
 };
+
+//TODO: define a reducer for the currentVideo field of our state.
 
 export default currentVideoReducer;
